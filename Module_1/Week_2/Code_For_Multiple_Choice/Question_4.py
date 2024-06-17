941536dd709e252110073aa8d45550544a2ed26f
Nha Tuyen <tuyenntn.ce190631@gmail.com>
2024-06-17 23:39:20 +0700

    push all exercises of week2 - module 1

---
+++ Module_1/Week_2/Code_For_Multiple_Choice/Question_4.py
@@ -0,0 +1,21 @@
+def levenshtein_distance(token1, token2):
+    # Your Code Here
+    distance = [[0 for _ in range(len(token2) + 1)] for _ in range(len(token1) + 1)]
+    # End Code Here
+    for i in range(len(token1) + 1):
+        distance[i][0] = i
+
+    for j in range(len(token2) + 1):
+        distance[0][j] = j
+
+    for i in range(1, len(token1) + 1):
+        for j in range(1, len(token2) + 1):
+            if token1[i-1] == token2[j-1]:
+                distance[i][j] = distance[i - 1][j - 1]
+            else:
+                distance[i][j] = min(distance[i - 1][j], distance[i][j - 1], distance[i - 1][j - 1]) + 1
+    return float(distance[len(token1)][len(token2)])
+
+
+assert levenshtein_distance("hi", "hello") == 4.0
+print(levenshtein_distance(" hola ", " hello "))
